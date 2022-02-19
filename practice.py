04e5e8ff875e42c1ebda8dbb4d22560c1150772e
adaickalavan <adaickalavan@gmail.com>
2022-02-19 18:25:09 -0500

    Add basic qiskit.

--- practice.py
+++ practice.py
@@ -1,5 +1,14 @@
 from qiskit import QuantumCircuit
-# Create quantum circuit with 3 qubits and 3 classical bits
-# (we'll explain why we need the classical bits later)
 qc = QuantumCircuit(3, 3)
-qc.draw()  # returns a drawing of the circuit
+# measure qubits 0, 1 & 2 to classical bits 0, 1 & 2 respectively
+qc.measure([0,1,2], [0,1,2])
+qc.draw()
+
+
+from qiskit.providers.aer import AerSimulator
+sim = AerSimulator()  # make new simulator object
+
+job = sim.run(qc)      # run the experiment
+result = job.result()  # get the results
+result.get_counts()    # interpret the results as a "counts" dictionary
+
