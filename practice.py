494b10f204bd8b5eaa9539d8d1d20fd7bd91c221
adaickalavan <adaickalavan@gmail.com>
2022-02-23 16:33:29 -0500

    Add basic qiskit.

--- practice.py
+++ practice.py
@@ -12,3 +12,8 @@ job = sim.run(qc)      # run the experiment
 result = job.result()  # get the results
 result.get_counts()    # interpret the results as a "counts" dictionary
 
+# Create quantum circuit with 3 qubits and 3 classical bits:
+qc = QuantumCircuit(3, 3)
+qc.x([0,1])  # Perform X-gates on qubits 0 & 1
+qc.measure([0,1,2], [0,1,2])
+qc.draw()    # returns a drawing of the circuit
