618b65c2183ebdaadcdb2a9278d29bc67fcbc296
adaickalavan <adaickalavan@gmail.com>
2024-01-21 14:00:44 -0500

    Add verifications.

--- amplitude_amplification.py
+++ amplitude_amplification.py
@@ -2,6 +2,7 @@ from qiskit import QuantumCircuit, QuantumRegister, execute, BasicAer
 from qiskit.visualization import array_to_latex
 from IPython.display import display
 from qiskit.circuit.library import MCMT
+from qiskit.quantum_info import Statevector
 
 # Set up the program
 n_qubits = 4
@@ -10,15 +11,11 @@ number_of_iterations = 2
 reg = QuantumRegister(n_qubits, name="reg")
 qc = QuantumCircuit(reg)
 qc.h(reg)
-x_bits = ~number_to_flip  # ~x is equivalent to (-x) - 1
+x_bits = ~number_to_flip  # Perform bit flip. Here, `~x` is equivalent to `(-x) - 1`.
 x_list = [reg[x] for x in range(n_qubits) if x_bits & (1 << x)]
 
 # Create multi controlled-z gate
 mtcx = MCMT(gate="cz", num_ctrl_qubits=n_qubits - 1, num_target_qubits=1)
-display(
-    mtcx.draw(output="mpl", style="iqp", cregbundle=False, initial_state=True, fold=-1)
-)
-
 
 # Create diffuser circuit
 def diffuser(n):
@@ -35,6 +32,10 @@ def diffuser(n):
 
     return gate
 
+# Verify statevector
+print(f"Statevector after 0 amplitude amplification iteration")
+ket = Statevector(qc)
+display(array_to_latex(ket, precision=3, prefix="\\psi =", max_size=2**n_qubits))
 
 # Amplitude amplification cycle
 for i in range(number_of_iterations):
@@ -48,14 +49,18 @@ for i in range(number_of_iterations):
     qc.barrier()
     qc.append(diffuser(n_qubits), range(n_qubits))
 
+    # Verify statevector
+    print(f"Statevector after {i+1} amplitude amplification iteration")
+    ket = Statevector(qc)
+    display(array_to_latex(ket, precision=3, prefix="\\psi =", max_size=2**n_qubits))
+
 # Run circuit
 backend = BasicAer.get_backend("statevector_simulator")
 job = execute(qc, backend)
 result = job.result()
 
+print("Final state probabilities")
 outputstate = result.get_statevector(qc, decimals=3)
-display(array_to_latex(outputstate, prefix="\\text{Statevector}", max_size=32))
-
 total_prob = 0
 for i, amp in enumerate(outputstate):
     if abs(amp) > 0.000001:
